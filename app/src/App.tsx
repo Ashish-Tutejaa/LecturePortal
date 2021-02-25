589b74e6d0a13d95af127088e14d335782769e5f
Ashish <ashish.tutejaa@gmail.com>
2021-02-25 21:18:36 +0700

    auth panel mockup made

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useRef} from 'react';
 import './App.css';
+import {TextField,Button} from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Login = () => {
+
+  const fileRef = useRef<null | HTMLInputElement>(null);
+
+  const textStyles = {
+    width: '70%',
+    margin: "5px 0px"
+  }
+  return <div className='authPanel'>
+  <div className='login'>
+    <h3>Sign In</h3>
+    <TextField style={textStyles} label="Username"/>
+    <TextField style={textStyles} label='Password'/>
+    <Button style={{margin: "30px 0px"}} size='small' variant='outlined' color='primary' children='sign in'/>
+  </div>
+  <div className='vr'></div>
+  <div className='signup'>
+    <h3>Sign Up</h3>
+    <TextField style={textStyles} label="Username"/>
+    <TextField style={textStyles} label='Password'/>
+    <TextField style={textStyles} label='Confirm Password'/>
+    <Button onClick={() =>{
+      if(fileRef.current !== null){
+        fileRef.current.click();
+      }
+    }} style={{margin: "15px 0px"}} size='small' variant='contained' color='default' children='Select ID Photo'/>
+    <Button size='small' variant='contained' color='primary' children='sign up'/>
+    <input ref={fileRef} type='file' hidden></input>
+  </div>
+  </div>
+}
+
+const App = () => {
+  return <div className='body-wrapper'>
+    <Login/>
+  </div>
 }
 
 export default App;
