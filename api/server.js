5c0400c9fe9310249420ea5c3fb275c129ce99a2
Ashish <ashish.tutejaa@gmail.com>
2021-02-28 01:02:16 +0700

    image transfer mockup

--- api/server.js
+++ api/server.js
@@ -1,1 +1,28 @@
-const express = require("express");
+const express = require('express');
+const port = 5000;
+const app = express();
+
+const CORS = (req, res, next) => {
+	res.set({
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Methods': '*',
+		'Access-Control-Allow-Headers': '*',
+	});
+	next();
+};
+
+app.use(express.json());
+app.use(express.raw());
+
+app.options('/getData', CORS);
+
+app.post('/getData', CORS, (req, res) => {
+	console.log(req.headers);
+	console.log(req.body);
+	console.log(typeof req.body);
+	res.status(200).end();
+});
+
+app.listen(port, () => {
+	console.log(`listening@${port}`);
+});
