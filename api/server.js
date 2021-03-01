47180b84c9c69074a6d04fec98b9dcff0f86d4fa
Ashish <ashish.tutejaa@gmail.com>
2021-03-01 18:36:36 +0700

    adding routers

--- api/server.js
+++ api/server.js
@@ -3,8 +3,7 @@ const axios = require('axios');
 const bodyParser = require('body-parser');
 const port = 5000;
 const app = express();
-const key = require('config.js')['OCIM_KEY'];
-const ep = require('config.js')['END_POINT'];
+const postImageRouter = require('./routers/postImage');
 
 const CORS = (req, res, next) => {
 	res.set({
@@ -22,30 +21,14 @@ app.use(
 	})
 );
 
-app.options('/getData', CORS);
-
-app.post('/getData', CORS, (req, res) => {
-	console.log(req.headers);
-	console.log(x.byteLength);
-	let sendingData = new Uint8Array(req.body);
-	axios({
-		method: 'POST',
-		url: ep,
-		data: sendingData,
-		headers: {
-			'Ocp-Apim-Subscription-Key': key,
-			'Content-type': 'application/octet-stream',
-		},
-	})
-		.then(res => {
-			console.log('RES');
-			console.log(res.data);
-		})
-		.catch(err => {
-			console.log('ERR');
-			console.log(err);
-		});
+//Data route
+//access: private
+//ep: /data
+//method: POST
+app.options('/data', CORS, (req, res) => {
+	res.status(200).end();
 });
+app.use('/data', CORS, postImageRouter);
 
 app.listen(port, () => {
 	console.log(`listening@${port}`);
